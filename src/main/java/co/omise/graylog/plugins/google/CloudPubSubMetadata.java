65a54815160faf95a3fb520e174235bebee68000
KongZ <goonohc@gmail.com>
2019-01-15 16:34:59 +0700

    bump version

--- src/main/java/co/omise/graylog/plugins/google/CloudPubSubMetadata.java
+++ src/main/java/co/omise/graylog/plugins/google/CloudPubSubMetadata.java
@@ -33,7 +33,7 @@ public class CloudPubSubMetadata implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(1, 0, 3));
+        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(1, 0, 4));
     }
 
     @Override
